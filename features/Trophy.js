46106d894c90f9a1e96541ae059ec812cae3989f
Perry5596 <jdbrennan07@gmail.com>
2024-12-17 18:35:49 -0600

    refactor Trophy.js to create a reusable function for rendering trophy waypoints

--- features/Trophy.js
+++ features/Trophy.js
@@ -7,25 +7,24 @@ import { registerWhen } from "../utils/functions";
 import { getWorld } from "../utils/world";
 import { colors } from "../utils/constants";
 
-// --------------------------------- Variables ---------------------------------
-
 // --------------------------------- Functions ---------------------------------
 
+function makeTrophyWaypoint(name, x, y, z) {
+    let distance = Math.floor(Math.sqrt(Math.pow(Player.getX() - x, 2)+ Math.pow(Player.getZ() - z, 2)));
+    // (x, y, z, w, h, red, green, blue, alpha, seeThroughWalls?)
+    RenderLib.drawEspBox(x, y, z, 1, 1, 0, 0.7, 0.90, 1, true);
+    RenderLib.drawInnerEspBox(x, y, z, 1, 1, 0, 0.7, 0.90, 0.5, false);
+    Tessellator.drawString(name, x, y + 1.5, z, colors.AQUA, true);
+    Tessellator.drawString(`${distance}m`, x, y + 1.25, z, colors.YELLOW, true);
+}
 // --------------------------------- Triggers ---------------------------------
 // Trophy Locations Render
 registerWhen(register("renderWorld", () => {
-    // (x, y, z, w, h, red, green, blue, alpha, seeThroughWalls?)
-    let x = -360;
-    let y = 82;
-    let z = -457;
-
-    let distance = Math.floor(Math.sqrt(Math.pow(Player.getX() - x, 2)+ Math.pow(Player.getZ() - z, 2)));
-
-    RenderLib.drawEspBox(x, y, z, 1, 1, 0, 0.7, 0.90, 1, true);
-    RenderLib.drawInnerEspBox(x, y, z, 1, 1, 0, 0.7, 0.90, 0.5, false);
-    Tessellator.drawString("Trophy Fish 1", x, y + 1.5, z, colors.AQUA, true);
-    Tessellator.drawString(`${distance}m`, x, y + 1.25, z, colors.YELLOW, true);
-
+    makeTrophyWaypoint("Flyfish/Volcanic", -355.5, 154, -840.5);
+    makeTrophyWaypoint("Moldfin", -268.5, 100, -750.5);
+    makeTrophyWaypoint("Skeleton", -470.5, 104, -756.5);
+    makeTrophyWaypoint("Soul", -295.5, 135, -558.5);
+    makeTrophyWaypoint("Karate", -221.5, 108, -569.5);
 }), () => settings.renderTrophyLocations && getWorld() == "Crimson Isle");
 
 

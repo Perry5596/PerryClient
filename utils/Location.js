dd206bb8c93694ee9ef24cd3cc9a03f6e6bdf49c
Perry5596 <jdbrennan07@gmail.com>
2024-12-26 14:43:30 -0600

    Add Location class to manage world loading and retrieval

---
+++ utils/Location.js
@@ -0,0 +1,36 @@
+// Credit: Adapted from Volcaddons findworld
+import { setRegisters } from "./functions";
+
+class Location {
+    #world = undefined;
+
+    constructor() {
+        register("worldLoad", () => {
+            this.findWorld();
+        }).setPriority(Priority.LOWEST);
+      
+        register("worldUnload", () => {
+            this.#world = undefined;
+            setRegisters((off = true));
+        });
+    }
+
+    getWorld() {
+        return this.#world;
+    }
+
+    findWorld = (noFind = 0) => {
+        // Make sure Hypixel world is loaded)
+        if (noFind > 9) return;
+        else if (!World.isLoaded()) delay(() => this.findWorld(noFind + 1), 1000);
+    
+        // Get world from tab list
+        let world = TabList.getNames()?.find((tab) => tab.startsWith("§r§b§lArea:") || tab.startsWith("§r§b§lDungeon:"));
+        if (world === undefined) delay(() => this.findWorld(noFind + 1), 1000);
+        else {
+          // Get world formatted
+          this.#world = world.removeFormatting().split(" ").splice(1).join(" ");
+        }
+      };
+}
+export default new Location();

ee3a9e7bb11af0f40926cca26cdce2cf2a9d84cb
Perry5596 <jdbrennan07@gmail.com>
2024-12-26 15:44:05 -0600

    Add booster_cookie property to data object in utils/data.js

--- utils/data.js
+++ utils/data.js
@@ -3,7 +3,8 @@ import PogObject from "PogData"
 export let data = new PogObject("Perry",{
     // Add in lines here for data you want to save:
     "first_time": true,
-    "version": "1.0.0"
+    "version": "1.0.0",
+    "booster_cookie": true
 }, "/data/data.json")
 
 register("gameUnload", () => {

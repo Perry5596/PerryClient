c03711bab804b8a1a57dcf3b05ac610293ecf750
Perry5596 <jdbrennan07@gmail.com>
2025-01-08 19:08:48 -0600

    Remove Ghost ESP trigger from General.js and clean up unused imports

--- features/General.js
+++ features/General.js
@@ -2,11 +2,9 @@ console.log("General.js is loading!"); // Debug
 // --------------------------------- Imports ---------------------------------
 import settings from "../config";
 import location from "../utils/Location";
-import RenderLib from "RenderLib";
 
 import { consts } from "../utils/constants";
 import { registerWhen } from "../utils/functions";
-import { SMA } from "../utils/constants";
 import { data } from "../utils/data";
 import { sendWebhook } from "../utils/webhook";
 
@@ -29,19 +27,6 @@ function triggerAlert(text, time) {
 registerWhen(register("chat", () => {
     triggerAlert("§c§lPIGGY CRACKED!", 1000);
 }).setCriteria("&cYou died and your piggy bank cracked!").setContains(), () => settings.piggyBankAlert);
-
-// Ghost ESP Trigger (Dwarven Mines GHOSTS)
-registerWhen(register("renderWorld", () => {
-    const GHOSTS = World.getAllEntitiesOfType(Java.type("net.minecraft.entity.monster.EntityCreeper").class);
-
-    GHOSTS.forEach(creeper => {
-        let maxHP = creeper.getEntity().func_110148_a(SMA.field_111267_a).func_111125_b();
-        if (maxHP == 1_000_000) {
-            RenderLib.drawEspBox(creeper.getRenderX(), creeper.getRenderY(), creeper.getRenderZ(), 0.6, 1.7, settings.ghostEspColor.getRed() / 255, settings.ghostEspColor.getGreen() / 255, settings.ghostEspColor.getBlue() / 255, settings.ghostEspColor.getAlpha(), false);
-            RenderLib.drawInnerEspBox(creeper.getRenderX(), creeper.getRenderY(), creeper.getRenderZ(), 0.6, 1.7, settings.ghostEspColor.getRed() / 255, settings.ghostEspColor.getGreen() / 255, settings.ghostEspColor.getBlue() / 255, settings.ghostEspColor.getAlpha() / 1000, false);
-        }
-    });
-}), () => settings.ghostESP && location.getWorld() == "Dwarven Mines");
 
 // Booster Cookie Notification Trigger
 registerWhen(register("postGuiRender", () => {
@@ -90,6 +75,9 @@ register("step", () => {
         afk = false;
     }
 }).setDelay(60);
+
+// Full Sack of Sacks Notification Trigger
+// &r&eYour Combat Sack &r&eis full of &r&fEnder Pearl&r&e!&r
 
 // Click to Visit Trigger
 registerWhen(
